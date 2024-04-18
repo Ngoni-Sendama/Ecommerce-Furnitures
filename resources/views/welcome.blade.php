37598ffe62b2f44301f96d08e0bca398be3abc42
Ngoni-Sendama <ngonidzashesendama@gmail.com>
2024-04-18 07:23:29 +0530

    Product and Category Controllers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -419,18 +419,18 @@
                         <img src="{{asset('front/img/client_logo/client_logo_3.png')}}" alt="">
                     </div>
 
-                    {{-- Last Line --}}
+               
                     <div class="single_client_logo">
-                        <img src="img/client_logo/client_logo_1.png" alt="">
+                        <img src="{{asset('front/img/client_logo/client_logo_1.png')}}" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="img/client_logo/client_logo_2.png" alt="">
+                        <img src="{{asset('front/img/client_logo/client_logo_2.png')}}" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="img/client_logo/client_logo_3.png" alt="">
+                        <img src="{{asset('front/img/client_logo/client_logo_3.png')}}" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="img/client_logo/client_logo_4.png" alt="">
+                        <img src="{{asset('front/img/client_logo/client_logo_4.png')}}" alt="">
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('category',[CategoryController::class, 'index'])->name('category');
+
+Route::get('product', [ProductController::class, 'index'])->name('products');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
